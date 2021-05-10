49aba1f41f20874afdf2ee947efa029530b79d03
Cao Binh <binhcao18@gmail.com>
2021-05-10 09:35:57 +0700

    xóa thẻ form và xóa lệnh thừa trên js

--- [Bai-tap]Ung-dung-doi-ten-may-tinh/caculator.js
+++ [Bai-tap]Ung-dung-doi-ten-may-tinh/caculator.js
@@ -1,29 +1,29 @@
-let numberone = document.getElementById('numberone').value;
-let numbertwo = document.getElementById('numbertwo').value; 
-let result = document.getElementById('result').value;
+// let numberone = document.getElementById('numberone').value;
+// let numbertwo = document.getElementById('numbertwo').value; 
+// let result = document.getElementById('result').value;
 function addition() {
-    result = document.getElementById('result').value;
+    // result = document.getElementById('result').value;
     numberone = parseInt(document.getElementById('numberone').value);
     numbertwo = parseInt(document.getElementById('numbertwo').value);
     result = (numberone + numbertwo);
     document.getElementById('result').innerText= ('Result Addition:') + (result);
 }
 function subtraction() {
-    result = document.getElementById("result").value;
+    // result = document.getElementById("result").value;
     numberone = parseInt(document.getElementById("numberone").value);
     numbertwo = parseInt(document.getElementById("numbertwo").value);
     result = numberone - numbertwo;
     document.getElementById("result").innerText = (result);
 }
 function multiplication() {
-    result = document.getElementById("result").value;
+    // result = document.getElementById("result").value;
     numberone = parseInt(document.getElementById("numberone").value);
     numbertwo = parseInt(document.getElementById("numbertwo").value);
     result = numberone * numbertwo;
     document.getElementById("result").innerText = (result);
 }
 function division() {
-    result = document.getElementById("result").value;
+    // result = document.getElementById("result").value;
     numberone = parseInt(document.getElementById("numberone").value);
     numbertwo = parseInt(document.getElementById("numbertwo").value);
     result = numberone / numbertwo;
